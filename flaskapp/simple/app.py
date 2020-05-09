8704baca9be2fb94fec4af885a1dc693414fdfb2
Ajith Kumar <ajith@knowesis.com>
2020-05-09 16:22:45 +1000

    Slides

--- flaskapp/simple/app.py
+++ flaskapp/simple/app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template('sample.html')
+
+@app.route('/test')
+def test():
+   return render_template('test.html')
+
 if __name__ == '__main__':
    app.run()
-
